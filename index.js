33e56c36d4391c8fd36db7d74ac7b83cf8b40dda
marcantoniosmap <marcantoniosmap@gmail.com>
2020-06-18 03:39:47 +0700

    from amar

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ const cors= require('cors');
 
 app.use(express.json());
 app.use(cors());
-dotenv.config();
 
+// MONGODB CONNECTION
 const mongodb = require('mongodb');
 const db = require('sharedb-mongo')({mongo: function(callback) {
-    mongodb.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, }, callback);
+    mongodb.connect('mongodb+srv://adminmarc:yGlabn12@cluster0-luwgd.mongodb.net/ShareDB?retryWrites=true&w=majority', { useUnifiedTopology: true, }, callback);
 }});
 
 const share = new ShareDB({ db });
 // const share = new ShareDB();
 const WebSocket = require('ws');
 const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
-// const ShareDBMingoMemory = require('sharedb-mingo-memory');
 
+dotenv.config();
 
 // temporary react base template for sharedb
 const fs = require('fs');
+const { response } = require('express');
+// REACT TEMPLATE
 var raw_data = fs.readFileSync('reactTemplate.json');
 var temp_data = JSON.parse(raw_data);
-
+// SHAREDB TEMPLATE
 var raw_data = fs.readFileSync('tempJ.json');
 var temp_json = JSON.parse(raw_data);
 
@@ -38,10 +40,9 @@ const GETPROJECTURL ='https://project.cogether.me/api/project/read/'
 // const share = new ShareDB({db}); // insert ShareDBMingoMemory later
 const server = http.createServer(app); const wss = new WebSocket.Server({ server: server })
 
-const port = process.env.PORT || 9000
+server.listen(9001);
 
-server.listen(port);
-
+// transforms reactTemplate to sandpack
 function getFile(content) {
     const dic = {};
     for (let i = 0; i < content.length; i++) {
@@ -54,28 +55,8 @@ function getFile(content) {
     return dic;
 }
 
-
-
-
-// console.log(temp_data);
-function replaceDots(content) {
-    const dic = {};
-    for (var key in content) {
-        // console.log(key.replace('.', '!^%!@'))
-    }
-}
-function reverseDots(content) {
-    const dic = {};
-    for (var key in content) {
-        // console.log(key.replace('!^%!@', '.'))
-        // console.log(content[key]);
-    }
-}
+// sandpack file
 temp_file = getFile(temp_data)
-
-var test = replaceDots(temp_file);
-var test2 = reverseDots(test);
-
 
 // connect any incoming websocket connection
 wss.on('connection', function(ws){
@@ -84,18 +65,8 @@ wss.on('connection', function(ws){
 });
 
 var connection = share.connect();
-var doc = connection.get('project', 'test1');
-doc.fetch(function (err) {
-    if (err) throw err;
-    if (doc.type === null) {
-        temp_file = getFile(temp_data);
-        // console.log(temp_json[0]);
-        doc.create(temp_json[0]); // shareDB cant use nested JSON }
-        // console.log(doc.data);
-    }
-}
-);
 
+// transforms sharedb file to sandpack 
 function transformJson(content) {
     var dic = {};
     for (var i = 0; i < content['file_idx'].length; i++) {
@@ -104,27 +75,8 @@ function transformJson(content) {
     return dic;
 }
 
-var new_dic = transformJson(temp_json[0]);
-// console.log(new_dic);
-// console.log(temp_json[0]['code'][0])
-
-
-
-app.get('/',async (req,res)=>{
-    const anjeng = await db.getCollection('project',function(err,collection){
-        try{
-            console.log(collection);
-            res.send({collection:collection});
-        }catch(err){
-            res.send({err:err});
-        }
-    });
-    // const response= share.collection.count();
-
-});
-
 app.get('/getFromShareDB/:id', async (req, res) => {
-    const project_id = req.params.id;
+    // gets project details from project backend
     const getProject = async (url, token) => {
         try {
             const project = await fetch(url,
@@ -142,40 +94,69 @@ app.get('/getFromShareDB/:id', async (req, res) => {
     }
     try {
         const response = await getProject(GETPROJECTURL + req.params.id, req.header('auth-token'));
-        res.send(response);
-    } catch (err) {
-        res.send({ err: err });
-    }
-    // if (project_id ){}
+        // console.log(response);
+        // console.log("AAAA");
+        var doc = connection.get('cogether', req.params.id);
+        doc.subscribe(function(err) {
+            if (err) throw err;
+        });
 
-    // connection.createFetchQuery(project_id, {}, {}, function (err, results) {
-    //     if (err) throw err;
-    //     if (results.length === 0) { // fetch document from project API
-    //         res.status(200).send(response);
-    //         response.source.forEach(file => {
-    //             var doc = connection.get(project_id, file.filename);
-    //             doc.create(file, function (err) {
-    //                 if (err) throw err;
-    //             });
-    //         });
+        // console.log("REFETCHED");
+        if (doc === null) { // project does not exist in sharedb
+            var share_data = convert2(response.source)
+            console.log(share_data);
+            doc.create(share_data); // sharedb file
+            // console.log(doc.data)
+            console.log("NEW DOC");
+            return res.send(response); // since the codes are the same
+        } else {
 
-    //     }
-    // });
+            // // use old JSON format but take files from doc.data; convert doc.data to oldJSON
+            // // doc.data => reactTemplate 
+            console.log(doc.data); // ga ngeload
+            var new_response = convert1(doc.data, response.source);
+            // console.log(doc.data);
+            // console.log(new_response);
+            response.source = new_response;
+            // console.log(response);
+            console.log("OLD DOC");
+
+            doc.destroy();
+
+            return res.send(response);
+        }
+        } catch (err) {
+            res.status(400).send({ err: err });
+        }
 });
 
-// initial document
-// connection.createFetchQuery('project', {}, {}, function(err, results) {
-//     if (err) throw err;
-//     if (results.length === 0) { // create initial data (?)
-//         temp_data.forEach(file => {
-//             var doc = connection.get('project', file.filename);
-//             doc.create(file, function(err) {
-//                 if (err) throw err;
-//             });
-//         });
-//     }
-// });
 
-// projectID
-// Index.js
-// UserList.js
+// doc.data => reactTemplate
+function convert1(content, react_temp) {
+    dic = []
+    for (let index = 0; index < content[0]['code'].length; index++) {
+        react_temp[index]['filename'] = content[0]['file_idx'][index];
+        react_temp[index]['code'] = content[0]['code'][index];
+    }
+    return react_temp
+}
+
+// var react_temp = convert1(temp_json, temp_data);
+
+// reactTemplate => doc.data
+function convert2(content) {
+    dic = [
+        {
+            "code": [],
+            "file_idx": []
+        }
+    ]
+    for (let index = 0; index < content.length; index++) {
+        var filename = (content[index]['filename']);
+        var code = (content[index]['code']);
+        dic[0]['file_idx'].push(filename);
+        dic[0]['code'].push(code);
+    }
+    return dic
+}
+// var doc_data = convert2(temp_data);
