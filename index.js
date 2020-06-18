4ca4d91767d8a0489c7ac1009d3f5e61a686750b
marcantoniosmap <marcantoniosmap@gmail.com>
2020-06-18 07:32:20 +0700

    allow origins

--- index.js
+++ index.js
@@ -4,81 +4,43 @@ const app = express();
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 const ShareDB = require('sharedb');
-const cors= require('cors');
-
-// var ShareDBMingo = require('sharedb-mingo-memory');
-// var db = new ShareDBMingo();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
 // MONGODB CONNECTION
 const mongodb = require('mongodb');
-const db = require('sharedb-mongo')({mongo: function(callback) {
-    mongodb.connect('mongodb+srv://adminmarc:yGlabn12@cluster0-luwgd.mongodb.net/ShareDB?retryWrites=true&w=majority', { useUnifiedTopology: true, }, callback);
-}});
+const db = require('sharedb-mongo')({
+    mongo: function (callback) {
+        mongodb.connect('mongodb+srv://adminmarc:yGlabn12@cluster0-luwgd.mongodb.net/ShareDB?retryWrites=true&w=majority', { useUnifiedTopology: true, }, callback);
+    }
+});
 
 const share = new ShareDB({ db });
-// const share = new ShareDB();
 const WebSocket = require('ws');
 const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
-
 dotenv.config();
 
-// temporary react base template for sharedb
-const fs = require('fs');
-const { response } = require('express');
-// REACT TEMPLATE
-var raw_data = fs.readFileSync('reactTemplate.json');
-var temp_data = JSON.parse(raw_data);
-// SHAREDB TEMPLATE
-var raw_data = fs.readFileSync('tempJ.json');
-var temp_json = JSON.parse(raw_data);
-
-const GETPROJECTURL ='https://project.cogether.me/api/project/read/'
+const GETPROJECTURL = 'https://project.cogether.me/api/project/read/'
 
 // const share = new ShareDB({db}); // insert ShareDBMingoMemory later
-const server = http.createServer(app); 
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server: server, 'Access-Control-Allow-Origin': '*' })
 
 const port = process.env.PORT || 9001
 
 server.listen(port);
 
-// transforms reactTemplate to sandpack
-function getFile(content) {
-    const dic = {};
-    for (let i = 0; i < content.length; i++) {
-        try {
-            dic[content[i].filename] = { code: content[i].code };
-        } catch (err) {
-            dic[content[i].filename] = { code: '' };
-        }
-    }
-    return dic;
-}
-
-// sandpack file
-temp_file = getFile(temp_data)
-
 // connect any incoming websocket connection
-wss.on('connection', function(ws){
+wss.on('connection', function (ws) {
     var stream = new WebSocketJSONStream(ws);
     share.listen(stream);
 });
 
 var connection = share.connect();
 
-// transforms sharedb file to sandpack 
-function transformJson(content) {
-    var dic = {};
-    for (var i = 0; i < content['file_idx'].length; i++) {
-        dic[content['file_idx'][i]] = {"code": content['code'][i]};
-    }
-    return dic;
-}
-
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
     res.send('here');
 })
 
@@ -101,40 +63,24 @@ app.get('/getFromShareDB/:id', async (req, res) => {
     }
     try {
         const response = await getProject(GETPROJECTURL + req.params.id, req.header('auth-token'));
-        // console.log(response);
-        // console.log("AAAA");
         var doc = connection.get('cogether', req.params.id);
-        doc.subscribe(function(err) {
+        doc.fetch(async function (err) {
             if (err) throw err;
+            if (doc.type === null) { // project does not exist in sharedb
+                var share_data = convert2(response.source)
+                const new_res = await doc.create(share_data); // sharedb file
+                console.log(response)
+                return res.send(response); // since the codes are the same
+            } else {
+                var new_response = convert1(doc.data, response.source);
+                response.source = new_response;
+                doc.destroy();
+                return res.send(response);
+            }
         });
-
-        // console.log("REFETCHED");
-        if (doc === null) { // project does not exist in sharedb
-            var share_data = convert2(response.source)
-            console.log(share_data);
-            doc.create(share_data); // sharedb file
-            // console.log(doc.data)
-            console.log("NEW DOC");
-            return res.send(response); // since the codes are the same
-        } else {
-
-            // // use old JSON format but take files from doc.data; convert doc.data to oldJSON
-            // // doc.data => reactTemplate 
-            console.log(doc.data); // ga ngeload
-            var new_response = convert1(doc.data, response.source);
-            // console.log(doc.data);
-            // console.log(new_response);
-            response.source = new_response;
-            // console.log(response);
-            console.log("OLD DOC");
-
-            doc.destroy();
-
-            return res.send(response);
-        }
-        } catch (err) {
-            res.status(400).send({ err: err });
-        }
+    } catch (err) {
+        res.status(400).send({ err: err });
+    }
 });
 
 
@@ -147,8 +93,6 @@ function convert1(content, react_temp) {
     }
     return react_temp
 }
-
-// var react_temp = convert1(temp_json, temp_data);
 
 // reactTemplate => doc.data
 function convert2(content) {
@@ -166,4 +110,3 @@ function convert2(content) {
     }
     return dic
 }
-// var doc_data = convert2(temp_data);
